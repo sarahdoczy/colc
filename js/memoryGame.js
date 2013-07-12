fbe1f7885ffa5eb2d744dced49c261c94a274d80
Sarah Doczy <sarahdoczy@gmail.com>
2013-07-12 08:52:45 -0700

    reformated alphabetLetters pages, resized memory game images and div panels, added functionality to memory game to shuffle deck, duplicate cards, display a certain number of cards on 'gameboard'.

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -1,90 +1,176 @@
-/*
-set number of cards 4 x 3 (6 images x 2)
-random placement of images each time game is played.
-user clicks 1 card, it turns over.
-if the card is already face-up, it is inactive (user can't click it).
-user can select up to 2 cards.
-when 2 cards are visible, determine if 2 cards are a match.
-if they match, cards stay flipped over.
-if they match, there is some "glittery surprise" (animation).
-if they don't match, cards flip back over and say "try again".
-once all cards have been matched (flipped over), animation occurs and a message appears saying "CONGRATULATIONS! Play again?"
+var createCard = function(image) {
 
-User clicks "Play Again" button.
-game refreshes - all cards flip back over => should be the same function that is initialized the initial game
-all cards are randomized again.
+    var img = document.createElement("img");
+    img.id="faceUpImage";
+    img.src=image;
 
+    var divFaceUp = document.createElement("div");
+    divFaceUp.className = 'faceUp';
+    divFaceUp.appendChild(img);
 
-A. if user doesn't click anything for 2 minutes, a message appears "please click a card" or whatever bullshit you want.
-B. should there be a hint button?
-C. after so many incorrect tries, the js could help find a match.
-D. time the game? => timer would start running as soon as the "Play Again" button is clicked.
-*/
+    var divFaceDown = document.createElement("div");
+    divFaceDown.className = 'faceDown';
 
+    var divPanel = document.createElement("div");
+    divPanel.className = 'panel';
+    divPanel.appendChild(divFaceDown);
+    divPanel.appendChild(divFaceUp);
+    divPanel.addEventListener('click', onCardClick, false);
 
-
-$(document).ready(function(){
-
-        $('.hover').click(function(){
-                $(this).addClass('flip');
-        },function(){
-                $(this).removeClass('flip');
-        });
-});
-
-
-var arrayOfImages = new Array();
-
-arrayOfImages.push({image:'images/memoryGameImages/black_rectangle.png',value: 'black rectangle'},
-                  {image:'images/memoryGameImages/blue_circle.png',value: 'blue circle'},
-                  {image:'images/memoryGameImages/blue_trapezoid.png',value: 'blue trapezoid'},
-                  {image:'images/memoryGameImages/blue_triangle.png',value: 'blue triangle'},
-                  {image:'images/memoryGameImages/brown_square.png',value: 'brown square'},
-                  {image:'images/memoryGameImages/gold_triangle.png',value: 'gold triangle'},
-                  {image:'images/memoryGameImages/green_diamond.png',value: 'green diamond'},
-                  {image:'images/memoryGameImages/green_pentagon.png',value: 'green pentagon'},
-                  {image:'images/memoryGameImages/green_rectangle.png',value: 'green rectangle'},
-                  {image:'images/memoryGameImages/grey_octagon.png',value: 'grey octagon'},
-                  {image:'images/memoryGameImages/orange_hexagon.png',value: 'orange hexagon'},
-                  {image:'images/memoryGameImages/orange_oval.png',value: 'orange oval'},
-                  {image:'images/memoryGameImages/pink_diamond.png',value: 'pink diamond'},
-                  {image:'images/memoryGameImages/pink_pentagon.png',value: 'pink pentagon'},
-                  {image:'images/memoryGameImages/pink_star.png',value: 'pink star'},
-                  {image:'images/memoryGameImages/purple_oval.png',value: 'purple oval'},
-                  {image:'images/memoryGameImages/purple_trapezoid.png',value: 'purple trapezoid'},
-                  {image:'images/memoryGameImages/red_circle.png',value: 'red circle'},
-                  {image:'images/memoryGameImages/red_octagon.png',value: 'red octagon'},
-                  {image:'images/memoryGameImages/white_hexagon.png',value: 'white hexagon'},
-                  {image:'images/memoryGameImages/yellow_square.png',value: 'yellow square'},
-                  {image:'images/memoryGameImages/yellow_star.png',value: 'yellow star'}
-                  );
-
-var getRandomNumber = function() {
-    return Math.floor(Math.random()*arrayOfImages.length)
+    return divPanel;
 }
 
-var getImage = function(index){
-    return arrayOfImages[index].image; // getRandomNumber is called here to grab a random image from the array. It's returning an image to this var.
+var onCardClick = function(event) {
+    flipCard(event.currentTarget);
+    setTimeout(runGameLogic, 1000, event.currentTarget);
+    numberOfClicks();
 }
 
-//get the value tied to the image that is being set
-var getImageValue = function(index){
-    return arrayOfImages[index].value;
+var count = 0;
+var numberOfClicks = function() {
+        count++;
+        //console.log(count);
+        if (count%2 == 0) {
+	    document.getElementById("numberOfTries").innerHTML = "Number of Attempts: " + count/2;
+       }
+}
+var clearNumberOfAttempts = function() {
+    //document.getElementById("numberOfTries").innerHTML = "Number of Attempts: ";
+    count = 0;
+    document.getElementById("numberOfTries").innerHTML = "Number of Attempts: ";
 }
 
-var setImage = function(image) {
-    //alert(image);
-    var imageValue = getImageValue(randomNumber);
-    document.getElementById("image").src = image;
-    document.getElementById("image").alt = imageValue;
+//TODO - "play again button" should clear current "#container", not add a new container.
+//we need to know when the game has ended (all cards have been flipped up)
+//user should not be able to click a "faceUp" card
+//user should not be able to have more than 2 faceUp cards at a time. (never have more than 2 faceUp cards at a time)
+//number of attempts should be cleared when "play Again" button is clicked.
+//make imgaes and squares smaller.
+
+
+var runGameLogic = function(card) {
+    if (previousClickedCard != null) {
+        var cardsAreEqual = compareCards(card, previousClickedCard);
+        if (cardsAreEqual) {
+            //console.log("It's a match");
+            previousClickedCard = null;
+        } else {
+            //console.log("Sorry try again");
+            flipCard(card);
+            flipCard(previousClickedCard);
+            previousClickedCard = null;
+        }
+    } else {
+        previousClickedCard = card;
+    }
 }
 
-var refresh = function(){
-    randomNumber = getRandomNumber();
-    setImage(getImage(randomNumber)); //getRandomImage is called to set the image in the HTML.
+function createGameBoard(imageArray, gameContainer) {
+    var cardDeck = chooseCardsToPlayWith(shuffle(imageArray));
+    var duplicatedCardsForGameBoard = getDuplicateArray(cardDeck); 
+    var shuffleDeckForGameBoard = shuffle(duplicatedCardsForGameBoard);
+        
+    for (var i=0; i<shuffleDeckForGameBoard.length; i++) {       
+        gameContainer.appendChild(createCard(shuffleDeckForGameBoard[i]));
+    }
 }
 
-refresh();
+var flipCard = function(div) {
+    if ($(div).hasClass('flip')) {
+        $(div).removeClass('flip');
+    } else {
+        $(div).addClass('flip');
+    }
+}
+
+var shuffle = function(array) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+var getDuplicateArray = function(array) {
+	var tempArray=[];
+	
+	for (var i=0; i<array.length; i++) {
+	    tempArray.push(array[i]);
+	    tempArray.push(array[i]); 
+	}
+	return tempArray;
+}
+
+var chooseCardsToPlayWith = function(array) {
+    var tempCardDeckArray = [];
+    
+    for (var i=0; i<numberOfCards; i++) { 
+        tempCardDeckArray.push(array[i]);
+    }
+    return tempCardDeckArray;
+}
+
+var compareCards = function(card1, card2) {
+    
+    //compare image source
+    var firstCard = card1.getElementsByClassName("faceUp")[0].getElementsByTagName("img")[0].src;
+    var secondCard = card2.getElementsByClassName("faceUp")[0].getElementsByTagName("img")[0].src;
+    if (firstCard == secondCard) {
+       return true;
+    } else {
+        return false;
+    
+    }
+}
+
+var onResetButtonClick = function(){
+    gameContainer.innerHTML = "";
+    clearNumberOfAttempts();
+    createGameBoard(imageArray, gameContainer);    
+}
+
+var numberOfCards = 6;  // chose the number of cards to play with (before duplicating array)
+var currentCard;
+var previousClickedCard;
+
+var imageArray = ['images/memoryGameImages/black_rectangle.png',
+                  'images/memoryGameImages/blue_circle.png',
+                  'images/memoryGameImages/blue_trapezoid.png',
+                  'images/memoryGameImages/blue_triangle.png', 
+                  'images/memoryGameImages/brown_square.png', 
+                  'images/memoryGameImages/gold_triangle.png', 
+                  'images/memoryGameImages/green_diamond.png',
+                  'images/memoryGameImages/green_pentagon.png', 
+                  'images/memoryGameImages/green_rectangle.png', 
+                  'images/memoryGameImages/grey_octagon.png', 
+                  'images/memoryGameImages/orange_hexagon.png', 
+                  'images/memoryGameImages/orange_oval.png', 
+                  'images/memoryGameImages/pink_diamond.png', 
+                  'images/memoryGameImages/pink_pentagon.png', 
+                  'images/memoryGameImages/pink_star.png', 
+                  'images/memoryGameImages/purple_oval.png', 
+                  'images/memoryGameImages/purple_trapezoid.png', 
+                  'images/memoryGameImages/red_circle.png', 
+                  'images/memoryGameImages/red_octagon.png', 
+                  'images/memoryGameImages/white_hexagon.png', 
+                  'images/memoryGameImages/yellow_square.png', 
+                  'images/memoryGameImages/yellow_star.png'
+                  ];
+
+var gameContainer = document.getElementById("gameContainer");
 
 
+createGameBoard(imageArray, gameContainer);
 
