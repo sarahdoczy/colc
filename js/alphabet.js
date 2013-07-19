4aa3ef107123461b7d4405bb4e5e94ad0539596a
Sarah Doczy <sarahdoczy@gmail.com>
2013-07-19 09:15:18 -0700

    fadeIn dynamic content

--- js/alphabet.js
+++ js/alphabet.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
   
      $("ul[class*=selectLetter] li").click(function () {
-                var letter = $(this).text(); // gets text contents of clicked li
-                $('.dynamicContent').load('alphabetLetters/'+letter+'.html');
-            });
+          var letter = $(this).text(); // gets text contents of clicked li
+          $('.dynamicContent').fadeOut(0).load('alphabetLetters/'+letter+'.html').fadeIn(800);
+     });
+  
 
-            
     $('select#alphabet').change(function () {
-        var letter = $('#alphabet option:selected').val();
-        $('.dynamicContent').load('alphabetLetters/'+letter+'.html');
+          var letter = $('#alphabet option:selected').val();
+          $('.dynamicContent').load('alphabetLetters/'+letter+'.html');
         
     }); 
 
-    //$('.alphabetContent').fadeIn(800);
-
-
 });
-
-    
